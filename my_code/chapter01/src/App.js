bc4c1578738d83a62fd0c312c80bc98881105b85
cadee247 <rousseaucadee@gmail.com>
2025-05-20 18:00:44 +0200

    frontend
    react

---
+++ my_code/chapter01/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'; 
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1 style={{ color: "purple" }}>Hi, my name is Cadee and I am a Fullstack Java Engineer!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
+//allows other files to be imported
